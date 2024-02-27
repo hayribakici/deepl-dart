d6aab6c0a1dc7a838c8bef71a072a76fa829b29e
Hayri Bakici <hayri.bakici@gmail.com>
2024-02-28 00:02:57 +0100

    initial commit

---
+++ bin/deepl_dart.dart
@@ -0,0 +1,5 @@
+import 'package:deepl_dart/deepl_dart.dart' as deepl_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${deepl_dart.calculate()}!');
+}
---
+++ test/deepl_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:deepl_dart/deepl_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
